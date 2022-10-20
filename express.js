0d3aeaeee9396d24d2def7f453c551e14ebb6028
Nkera Urbain <urbainmuhamiriza1990@gmail.com>
2022-10-20 15:15:13 +0200

    firt commit

--- express.js
+++ express.js
@@ -5,16 +5,16 @@ const date= new Date()
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
   console.log(__dirname)
-  res.sendFile( __dirname+"/accueil.html");
+  res.sendFile( __dirname+"/smallSite/accueil.html");
 });
 app.get('/p2', function(req, res) {
   console.log(__dirname)
-  res.sendFile( __dirname+"/pageCent.html");
+  res.sendFile( __dirname+"/smallSite/pageCent.html");
   
 });
 app.get('/p3', function(req, res) {
   console.log(__dirname)
-  res.sendFile( __dirname+"/pagefin.html");
+  res.sendFile( __dirname+"/smallSite/pagefin.html");
   
 });
 
